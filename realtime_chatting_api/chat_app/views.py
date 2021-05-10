e446d765748b5bc7d49b4c518877d4980e38b26e
maen08 <2001stany@gmail.com>
2021-05-10 20:29:44 +0300

    add: djangorest-api

---
+++ realtime_chatting_api/chat_app/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.contrib.auth import User
+from rest_framework import status
+from rest_framework.exceptions import AuthenticationFailed
+
+
+def register(request):
+    email = request.data.get('email')
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    if User.objects.filter(username=username).exists():
+        raise AuthenticationFailed('Username already exist!')
+
+    User.objects.create(
+        email=email,
+        username=username,
+        password=password
+    )
+
+
+    response = {
+        'username': username,
+        'status': status.HTTP_,
+        'data': 'Login to obtain API token'
+    }
+
+    return JsonResponse()
